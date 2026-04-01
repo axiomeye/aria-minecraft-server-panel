e31ef51c89dba8b9d616193da0f79ac484457e9f
Eugenio Guarino <eugenio-97@live.it>
2026-04-02 01:40:46 +0200

    chore: pin action SHAs, upgrade google provider to v7, enable IAP, pin terraform to 1.14.8

--- terraform/cloud-run/backend.tf
+++ terraform/cloud-run/backend.tf
@@ -4,12 +4,12 @@ provider "google" {
 }
 
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.10.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.0"
+      version = ">= 7.0"
     }
   }
 
